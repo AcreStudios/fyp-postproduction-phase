fbf01e99b9811364b40e047931e4fedfbf5f0b91
zlurker <tallspin_97@hotmail.com>
2017-02-22 18:09:09 +0800

    AI Cleanup
    -Trail Effect fixed.
    -AI child cleanup.
    -Ragdoll ect set up for AI.

--- Gold Phase/Assets/Scripts/AI/TrailEffectFade.cs
+++ Gold Phase/Assets/Scripts/AI/TrailEffectFade.cs
@@ -5,24 +5,34 @@ public class TrailEffectFade : MonoBehaviour {
 
     // Use this for initialization
     public Renderer toGetColor;
+    public Transform startPoint;
+    public Vector3 endPoint;
     public float rateOfDecay;
+    public float speed;
     float currentA;
 
     void Start() {
         toGetColor = GetComponent<Renderer>();
 
     }
-    public void ObjectActive() {
+    public void ObjectActive(Vector3 destination) {
         currentA = 1;
+        endPoint = destination;
         gameObject.SetActive(true);
     }
 
     void Update() {
         if (currentA > 0) {
-            currentA -= rateOfDecay;
-            toGetColor.material.color = new Color(1,1,1,currentA);
-        } else
+            transform.position = Vector3.MoveTowards(transform.position, endPoint, speed);
+
+            if (transform.position == endPoint) {
+                currentA -= rateOfDecay;
+                toGetColor.material.color = new Color(1, 1, 1, currentA);
+            }
+        } else {
+            transform.position = startPoint.position;
             gameObject.SetActive(false);
-          
+        }
+
     }
 }
--- Gold Phase/Assets/Scripts/ExperimentalScripts(Gab)/ColliderExperimentDirection.cs
+++ Gold Phase/Assets/Scripts/ExperimentalScripts(Gab)/ColliderExperimentDirection.cs
@@ -14,32 +14,17 @@ public class ColliderExperimentDirection : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (CheckCollider(colliders, target.position, startingPoint.position))
+        if (CheckCollider(colliders,  startingPoint.position - target.position))
             Debug.DrawLine(startingPoint.position, target.position, Color.black);
     }
 
-    bool CheckCollider(Collider[] colliders, Vector3 targetPos, Vector3 startPos) {
-        float targetGradient = CalculateGradient(startPos - targetPos);
+    bool CheckCollider(Collider[] colliders, Vector3 givenVector) {
+        Vector3 gradient = Mathf.Abs(givenVector.x) >= Mathf.Abs(givenVector.z) ? givenVector / Mathf.Abs(givenVector.x) : givenVector / Mathf.Abs(givenVector.z);
+        Debug.Log(gradient.x + gradient.z);
+
         foreach (Collider collider in colliders) {
-            Vector3 temp = Vector3.zero;
-
-            temp = collider.bounds.center - target.position;
-
-            temp.x = (temp.x / Mathf.Abs(temp.x)) * collider.bounds.extents.x;
-            temp.z = (temp.z / Mathf.Abs(temp.z)) * collider.bounds.extents.z;
-
-            temp.x *= -1;
-            Debug.DrawLine(startPos, collider.bounds.center + temp, Color.red);
-            Debug.DrawLine(startPos, collider.bounds.center - temp, Color.red);
-            Debug.Log(targetGradient);
-            Debug.Log(CalculateGradient(startPos - collider.bounds.center + temp));
-            Debug.Log(CalculateGradient(startPos - collider.bounds.center - temp));
-
-            if (CalculateGradient(startPos - collider.bounds.center + temp) >= targetGradient && targetGradient >= CalculateGradient(startPos - collider.bounds.center - temp))
-                return false;
 
         }
-
         return true;
     }
 
